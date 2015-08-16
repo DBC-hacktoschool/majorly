0b82de47c21faa361a0b40baea587db7c3fa6ee9
Andrew Deal <deal.e.andrew@gmail.com>
2015-08-16 11:09:47 -0700

    Updated script file

---
+++ public/quiz.js
@@ -0,0 +1,46 @@
+var questions = [
+                'Which of the following would you do?',
+                'Church-key meditation master cleanse deep v selfies selvage?',
+                'Sartorial cray fashion axe, mustache freegan pop-up normcore slow-carb art party Shoreditch twee wayfarers next level biodiesel?',
+                'Literally occupy street art Blue Bottle ugh, gluten-free meh readymade sriracha?',
+                'This is the end?'
+                ]
+
+
+var answers = [
+              [
+                'Draw a picture',
+                'Think about the meaning of life',
+                'Solve a puzzle',
+                'Make a million bucks',
+                'oiwejij'
+              ],
+              [
+                'Readymade',
+                'meh',
+                'Sartorial',
+                'heard',
+                'ennui'
+              ],
+              [
+                'Shoreditch',
+                'cliche',
+                'them',
+                'stumptown',
+                'kogi'
+              ],
+              [
+                'asymmetrical',
+                'meditation',
+                'locavore',
+                'normcore',
+                'master'
+              ],
+              [
+                'meditation',
+                'selfies',
+                'Marfa',
+                'kogi',
+                'Bottle'
+              ]
+              ];
--- public/script.js
+++ public/script.js
@@ -24,9 +24,7 @@ function renderLogo () {
 
 //Function to empty container and display first question
 function startQuiz() {
-  var msg = '<h1 class="intro-header"> Let gets to know you better </h1>'
-  var question1 = '<h1 class="question">Which of the following would you rather to do?</h1>'
-  var answers;
+  var msg = '<p class="intro-header"> Let\'s get to know you better </p>'
 
   //Clear image and container
   $('.container img').delay(3000).fadeOut(3000, function () {
@@ -36,23 +34,87 @@ function startQuiz() {
     //Display intro text and lead into first question
     $('.container').append(msg).hide().fadeIn(2000, function() {
 
-      showQ();
+      makeQ();
 
     });
   })
 }
 
 //Function will render the quiz of questions.
-function showQ () {
+function makeQ () {
 
-  //Lead into first quesiton
+
+  //Create and hide all questions
   $('.intro-header').fadeOut(3000, function() {
 
-    //
-    $('.container').empty().append(question1).hide().fadeIn(3000)
+    //Empty the container to allow for the questions
+    $('.container').empty();
+
+    _.each(questions, function (question, index) {
+
+      $('.container').append(function(){
+
+
+        return '<div id="' + index + '">' + question + '<hr></div>'
+
+      });
+
+      $('#' + index).hide()
+
+      _.each(answers, function (answerList, i) {
+
+        _.each(answerList, function (answer, j){
+
+          if (i === index) {
+
+            $('#' + index).append(function(){
+
+              if (j === 0) {
+
+                return '<button style="margin: 10px 0 10px 0">' + answer + '</button>' + '<br>';
+
+              }
+
+              return '<button>' + answer + '</button>' + '<br>';
+
+            });
+
+          }
+        })
+      })
+    })
+
+    showQ();
+
+  })
+}
+
+
+//Function will render first question and then subsequent questions based on click event
+function showQ () {
+
+  //Initialize counter variable to match index of questions array
+  var counter = 0;
+  var score = 0;
+
+  //Show first question
+  $('#' + counter).fadeIn(2000);
+
+  //Click event for user response
+  $('button').on('click', function (event) {
+
+    counter += 1;
+
+    
+
+    $('#' + (counter - 1)).remove()
+
+    $('#' + counter).fadeIn(1000);
+
 
 
   })
+
 
 
 }
